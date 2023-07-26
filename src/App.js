07d5a348f7a4cd661c3a1f2b53d5af62fc6fbe83
naomi230 <naomikuguru20@gmail.com>
2023-07-26 18:33:09 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to My To-Do List App</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a simple to-do list application built with React.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://example.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out my portfolio
         </a>
       </header>
     </div>
